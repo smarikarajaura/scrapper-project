bca15b003892262d5a11fc0f9d4e1f299850b75e
smarikarajaura <135974022+smarikarajaura@users.noreply.github.com>
2025-03-19 14:58:37 +0545

    complete project

---
+++ scrapper.py
@@ -0,0 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+URL = "http://books.toscrape.com/"
+
+
+def  scrape_books(url):
+    response =requests.get(url)
+    # print(response)
+    # print(response.status_code)
+    # print(response.text)
+    if response.status_code !=200:
+        print("Failed to fetch the page")
+        return
+    
+    response.encoding = response.apparent_encoding
+    soup = BeautifulSoup(response.text,"html.parser")
+    books = soup.find_all("article",class_="product_pod")
+
+    book_list = []
+    for book in books:
+        title=(book.h3.a["title"])
+        price_text = book.find("p",class_="price_color").text
+        currency = price_text[0]
+        price = price_text [1:]
+        print(title, currency,price)
+
+        book_list.append({
+            "title":title,
+            "currency":currency,
+            "price":price
+        })
+
+    with open("books.json","w",encoding="utf-8")as file:
+        json.dump(book_list,file,indent =4,ensure_ascii = False)
+    
+
+    print("Data saved to books.json")
+scrape_books(URL)
